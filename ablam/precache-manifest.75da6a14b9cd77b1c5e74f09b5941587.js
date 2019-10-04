0298ab58b11bb973f8b94531091a85cb5c529e85
cangokceaslan <cangokceaslanmail@gmail.com>
2019-10-05 02:55:29 +0300

    v1.0
    v1.0

---
+++ ablam/precache-manifest.75da6a14b9cd77b1c5e74f09b5941587.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4512ffe1ce22474d11600a66c88077c",
+    "url": "/ablam/index.html"
+  },
+  {
+    "revision": "2b21e2d72b10d1654bd9",
+    "url": "/ablam/static/css/main.61ab8a10.chunk.css"
+  },
+  {
+    "revision": "389acb3c438b6b5ce216",
+    "url": "/ablam/static/js/2.615f6e6e.chunk.js"
+  },
+  {
+    "revision": "2b21e2d72b10d1654bd9",
+    "url": "/ablam/static/js/main.8e8e7ea9.chunk.js"
+  },
+  {
+    "revision": "308b4b621d644f7765fb",
+    "url": "/ablam/static/js/runtime-main.b1559709.js"
+  }
+]);
--- ablam/service-worker.js
+++ ablam/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45095e77c177938b4cde6f7a001aaf76.js"
+  "/ablam/precache-manifest.75da6a14b9cd77b1c5e74f09b5941587.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ablam/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
