2b640c809d4b67e911aaa9c8d4b117abd91166ce
cangokceaslan <cangokceaslanmail@gmail.com>
2019-10-05 02:00:44 +0300

    v1.0
    v1.0

---
+++ ablam/precache-manifest.45095e77c177938b4cde6f7a001aaf76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf248919c0c875e710fea7da12aa9b86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4fba68e527b793e5038",
+    "url": "/static/css/main.2533663c.chunk.css"
+  },
+  {
+    "revision": "389acb3c438b6b5ce216",
+    "url": "/static/js/2.615f6e6e.chunk.js"
+  },
+  {
+    "revision": "e4fba68e527b793e5038",
+    "url": "/static/js/main.5441778a.chunk.js"
+  },
+  {
+    "revision": "2deeba57fc842088eb2c",
+    "url": "/static/js/runtime-main.9ed197ea.js"
+  }
+]);
