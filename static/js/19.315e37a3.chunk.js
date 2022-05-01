fcf4a0b7abf017fa5fa7c7b8723de19663e53196
Can Gökçeaslan <48398625+cangokceaslan@users.noreply.github.com>
2022-05-02 01:19:25 +0300

    Some major updates

---
+++ static/js/19.315e37a3.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcangokceaslan=this.webpackJsonpcangokceaslan||[]).push([[19],{106:function(a,t,n){"use strict";n.d(t,"a",(function(){return r}));var c=n(0);function r(a){return Object(c.a)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"}}]})(a)}}}]);
+//# sourceMappingURL=19.315e37a3.chunk.js.map
