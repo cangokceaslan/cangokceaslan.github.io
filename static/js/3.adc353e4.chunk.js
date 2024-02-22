368efbf630ed60ee23a3071da11429ce95ef9781
Can Gökçeaslan <48398625+cangokceaslan@users.noreply.github.com>
2024-02-22 13:48:01 +0300

    Updated GA formation

---
+++ static/js/3.adc353e4.chunk.js
@@ -0,0 +1,1 @@
+(this.webpackJsonpcangokceaslan=this.webpackJsonpcangokceaslan||[]).push([[3],{126:function(n,t,a){"use strict";a.d(t,"a",(function(){return o}));var e=a(0);function o(n){return Object(e.a)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"M368 368L144 144m224 0L144 368"}}]})(n)}}}]);
